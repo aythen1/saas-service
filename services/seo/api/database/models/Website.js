c45b28270255ae1fe487946b58b1f9c83b8ea6e6
Mario Caballero <mario.caballero.948@gmail.com>
2023-08-03 11:35:09 -0300

    fix: website model

---
+++ services/seo/api/database/models/Website.js
@@ -0,0 +1,55 @@
+import { DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  sequelize.define("Website", {
+    domain: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    privacy: {
+      type: DataTypes.ENUM("private", "public", "password"),
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    notification: {
+      type: DataTypes.BOOLEAN,
+    },
+    ips: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    url: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    bots: {
+      type: DataTypes.BOOLEAN,
+    },
+    script: {
+      type: DataTypes.STRING,
+    },
+    pages: {
+      type: DataTypes.INTEGER,
+    },
+    referrers: {
+      type: DataTypes.INTEGER,
+    },
+    countries: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    browsers: {
+      type: DataTypes.STRING,
+    },
+    operatingSystems: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    events: {
+      type: DataTypes.STRING,
+    },
+    visitors: {
+      type: DataTypes.INTEGER,
+    },
+    pageViews: {
+      type: DataTypes.INTEGER,
+    },
+  });
+};
